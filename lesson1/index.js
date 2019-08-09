feeab10aa8a1cf9ffc0d5e98de017cf897e94c6e
newliar <1107325513@qq.com>
2019-08-09 14:57:36 +0800

    commit vue introduction

--- lesson1/index.js
+++ lesson1/index.js
@@ -29,3 +29,42 @@ var app4 = new Vue({
         ]
     }
 })
+
+var app5 = new Vue({
+    el: '#app-5',
+    data: {
+        message: 'Hello Vue.js!'
+    },
+    methods: {
+        reverseMessage: function () {
+            this.message = this.message.split('').reverse().join('')
+            
+        }
+    }
+})
+
+var app6 = new Vue({
+    el: '#app-6',
+    data: {
+        message: 'Hello Vue!'
+    }
+})
+
+Vue.component('todo-item', {
+    // todo-item组件现在接受一个
+    // "prop"，类似于一个自定义特性。
+    // 这个 prop 名为 todo。
+    prop: ['todo'],
+    template: '<li>{{ todo.text }}</li>'
+})
+
+var app7 = new Vue({
+    el: '#app-7',
+    data: {
+        groceryList: [
+            { id: 0, text: '蔬菜'},
+            { id: 1, text: '奶酪'},
+            { id: 2, text: '随便其它什么人吃的东西'}
+        ]
+    }
+})
